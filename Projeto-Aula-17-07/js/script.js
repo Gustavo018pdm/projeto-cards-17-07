7cb90ffe34c40e829e812a2529df7fe612bdb814
gustavo nascimento <gustavopopo387@gmail.com>
2026-07-23 21:50:57 -0300

    mais um commit

---
+++ Projeto-Aula-17-07/js/script.js
@@ -0,0 +1,15 @@
+function trocarVenus(){
+    document.querySelector(".card-Mercurio").src="./Projeto-Aula-17-07/img/Venus.png"
+}
+
+function trocarMarte(){
+    document.querySelector(".card-Terra").src="./Projeto-Aula-17-07/img/Marte.PNG"
+}
+
+function trocarSaturno(){
+    document.querySelector(".card-Jupiter").src="./Projeto-Aula-17-07/img/Saturno.webp"
+}
+
+function trocarNetuno(){
+    document.querySelector(".card-Urano").src="./Projeto-Aula-17-07/img/Netuno.PNG"
+}
